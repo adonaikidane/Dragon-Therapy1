cc6218651644fd4f6328a6348b6fe68467110a2b
Chikanma Okoisor <Chikanma.okoisor@gmail.com>
2025-09-21 05:39:53 -0500

    port error

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 // Configure CORS to accept requests from your GitHub Pages URL
 const corsOptions = {
